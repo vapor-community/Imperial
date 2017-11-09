37991ec24b17cd8032438fd9fd693295d21dac2d
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-09 10:09:04 -0600

    Installed VaporOAuth package

--- Package.swift
+++ Package.swift
@@ -1,28 +1,17 @@
 // swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Imperial",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Imperial",
-            targets: ["Imperial"]),
+        .library(name: "Imperial", targets: ["Imperial"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/brokenhandsio/vapor-oauth.git", .exact("0.6.0"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Imperial",
-            dependencies: []),
-        .testTarget(
-            name: "ImperialTests",
-            dependencies: ["Imperial"]),
+        .target(name: "Imperial", dependencies: ["VaporOAuth"]),
+        .testTarget(name: "ImperialTests", dependencies: ["Imperial"]),
     ]
 )
