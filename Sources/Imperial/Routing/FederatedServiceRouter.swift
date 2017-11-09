aa9d402c73247ae87382477b95f1d6c50246a7d1
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-09 11:43:59 -0600

    Updated usage comment for FederatedServiceRouter protocol

--- Sources/Imperial/Routing/FederatedServiceRouter.swift
+++ Sources/Imperial/Routing/FederatedServiceRouter.swift
@@ -4,18 +4,24 @@ import Vapor
  Usage:
  
  class Router: FederatedServiceRouter {
-    var service: FederatedLoginService
-    var callbackURL: String
-    var authURL: String {
-        return "https://service.com/login/oauth/authorize?scope=user:email&client_id=" +
-                service.clientID
-    }
+     var service: FederatedLoginService
+     var callbackURL: String
+     var accessTokenURL: String = "https://service.com/login/oauth/access_token"
+     var authURL: String {
+         return "https://service.com/login/oauth/authorize?scope=user:email&client_id=" +
+                 service.clientID
+     }
  
      init(callback: String)throws {
          self.service = try Service()
          self.callbackURL = callback
      }
-  }
+ 
+     func callback(_ request: Request) -> ResponseRepresentable {
+         // Get the `code` value from the request and use it to get the `access_token`.
+         // See here for a Ruby implimentation: https://developer.github.com/v3/guides/basics-of-authentication/#providing-a-callback
+     }
+ }
  */
 
 protocol FederatedServiceRouter {
