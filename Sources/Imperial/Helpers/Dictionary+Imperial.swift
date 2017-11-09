98aec378fd3a40bbf978be78c96455db379575f1
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-09 15:09:27 -0600

    Added missing space separator for Dictionary.merge

--- Sources/Imperial/Helpers/Dictionary+Imperial.swift
+++ Sources/Imperial/Helpers/Dictionary+Imperial.swift
@@ -1,5 +1,5 @@
 extension Dictionary where Key == String, Value == String {
     func merge(with separator: String) -> String {
-        return self.reduce(into: "") { $0 += [$1.key, separator, $1.value].joined() }
+        return self.reduce(into: "") { $0 += [$1.key, separator, $1.value].joined(separator: " ") }
     }
 }
