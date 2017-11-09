316b19e2e60e5f562001b7d3cb8f5d018deb4f9a
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-09 15:08:27 -0600

    Made Dictionary.merge more performant and fixed `separator` argument name

--- Sources/Imperial/Helpers/Dictionary+Imperial.swift
+++ Sources/Imperial/Helpers/Dictionary+Imperial.swift
@@ -1,5 +1,5 @@
 extension Dictionary where Key == String, Value == String {
-    func merge(with seperator: String) -> String {
-        return self.map({ "\($0)\(seperator)\($1)" }).joined(separator: " ")
+    func merge(with separator: String) -> String {
+        return self.reduce(into: "") { $0 += [$1.key, separator, $1.value].joined() }
     }
 }
