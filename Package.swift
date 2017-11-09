4bf93784e549abf4d5bf720e1c55473e124abb23
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-09 11:59:54 -0600

    Installed FluentProvider package

--- Package.swift
+++ Package.swift
@@ -8,10 +8,11 @@ let package = Package(
         .library(name: "Imperial", targets: ["Imperial"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/brokenhandsio/vapor-oauth.git", .exact("0.6.0"))
+        .package(url: "https://github.com/brokenhandsio/vapor-oauth.git", .exact("0.6.0")),
+        .package(url: "https://github.com/vapor/fluent-provider.git", .exact("1.3.0"))
     ],
     targets: [
-        .target(name: "Imperial", dependencies: ["VaporOAuth"]),
+        .target(name: "Imperial", dependencies: ["VaporOAuth", "FluentProvider"]),
         .testTarget(name: "ImperialTests", dependencies: ["Imperial"]),
     ]
 )
