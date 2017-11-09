f8be76cff82a1a2c6531c6d709eb37fb6f0cc196
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-09 14:47:59 -0600

    Created GitHubRouter service router class

---
+++ Sources/Imperial/Services/GitHub/GitHubRouter.swift
@@ -0,0 +1,22 @@
+import Vapor
+
+public class GitHubRouter: FederatedServiceRouter {
+    public let service: FederatedLoginService
+    public var scope: [String: String] = [:]
+    public let callbackURL: String
+    public let accessTokenURL: String = "https://github.com/login/oauth/access_token"
+    public var authURL: String {
+        return "https://github.com/login/oauth/authorize?" +
+               "scope=\(scope.merge(with: ":"))&" +
+               "client_id=\(self.service.clientID)"
+    }
+    
+    public required init(callback: String) throws {
+        self.service = try GitHubAuth()
+        self.callbackURL = callback
+    }
+    
+    public func callback(_ request: Request) -> ResponseRepresentable {
+        return Response(redirect: "/")
+    }
+}
