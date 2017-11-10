05f6bf7b0eb9fd6d77888b335876fb57a823faee
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-10 07:33:28 -0600

    Created GitHub FederatedService class

---
+++ Sources/Imperial/Services/GitHub/GitHub.swift
@@ -0,0 +1,11 @@
+public class GitHub: FederatedService {
+    public var auth: FederatedLoginService
+    public var router: FederatedServiceRouter
+    
+    public required init(authenticate: String, callback: String, completion: @escaping (String) -> ()) throws {
+        self.router = try GitHubRouter(callback: callback, completion: completion)
+        self.auth = self.router.service
+        
+        try self.router.configureRoutes(withAuthURL: authenticate)
+    }
+}
