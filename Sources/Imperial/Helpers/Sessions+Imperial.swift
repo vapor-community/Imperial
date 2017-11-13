a43695eb114c5e2664fc5e14b166008040f14dd2
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-13 11:02:32 -0600

    Created Request.getAccessToken exyension method

--- Sources/Imperial/Helpers/Sessions+Imperial.swift
+++ Sources/Imperial/Helpers/Sessions+Imperial.swift
@@ -1,6 +1,12 @@
 import Vapor
 import Sessions
 
+extension Request {
+    public func getAccessToken()throws -> String {
+        return try self.assertSession().getAccessToken()
+    }
+}
+
 extension Session {
     public func getAccessToken()throws -> String {
         guard let token = self.data["access_token"]?.string else {
