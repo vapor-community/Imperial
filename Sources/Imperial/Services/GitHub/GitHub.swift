ddd440585a65478bffb1a907a6c46132e3cdec11
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-13 06:48:42 -0600

    Added router.scope set to GitHub init

--- Sources/Imperial/Services/GitHub/GitHub.swift
+++ Sources/Imperial/Services/GitHub/GitHub.swift
@@ -5,10 +5,11 @@ public class GitHub: FederatedService {
     public var router: FederatedServiceRouter
     
     @discardableResult
-    public required init(authenticate: String, callback: String, completion: @escaping (String) -> (ResponseRepresentable)) throws {
+    public required init(authenticate: String, callback: String, scope: [String: String] = [:], completion: @escaping (String) -> (ResponseRepresentable)) throws {
         self.router = try GitHubRouter(callback: callback, completion: completion)
         self.auth = self.router.service
         
+        self.router.scope = scope
         try self.router.configureRoutes(withAuthURL: authenticate)
     }
 }
