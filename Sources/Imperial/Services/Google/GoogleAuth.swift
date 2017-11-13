9407b2d9bd6a50c0f5b0981a5db260942a85f6c0
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-13 14:39:14 -0600

    Created GoogleAuth federated login service class

---
+++ Sources/Imperial/Services/Google/GoogleAuth.swift
@@ -0,0 +1,31 @@
+import Vapor
+
+public class GoogleAuth: FederatedLoginService {
+    public var idEnvKey: String = "GOOGLE_CLIENT_ID"
+    public var secretEnvKey: String = "GOOGLE_CLIENT_SECRET"
+    public var clientID: String
+    public var clientSecret: String
+    
+    public required init() throws {
+        let idError = ImperialError.missingEnvVar(idEnvKey)
+        let secretError = ImperialError.missingEnvVar(secretEnvKey)
+        
+        do {
+            guard let id = ImperialConfig.googleID else {
+                throw idError
+            }
+            self.clientID = id
+        } catch {
+            self.clientID = try Env.get(idEnvKey).value(or: idError)
+        }
+        
+        do {
+            guard let secret = ImperialConfig.googleSecret else {
+                throw secretError
+            }
+            self.clientSecret = secret
+        } catch {
+            self.clientSecret = try Env.get(secretEnvKey).value(or: secretError)
+        }
+    }
+}
