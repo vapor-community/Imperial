79ffdd6560e11b83f0158b7b2dd54f0a785d493e
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-13 06:37:59 -0600

    Made GitHub init discardableResult

--- Sources/Imperial/Services/GitHub/GitHub.swift
+++ Sources/Imperial/Services/GitHub/GitHub.swift
@@ -2,6 +2,7 @@ public class GitHub: FederatedService {
     public var auth: FederatedLoginService
     public var router: FederatedServiceRouter
     
+    @discardableResult
     public required init(authenticate: String, callback: String, completion: @escaping (String) -> ()) throws {
         self.router = try GitHubRouter(callback: callback, completion: completion)
         self.auth = self.router.service
