d309ae50ee0b72d03c487527b3b328992943ee7d
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-13 11:39:39 -0600

    Added redirecting to ImperialMIddleware

--- Sources/Imperial/Middleware/ImperialMiddleware.swift
+++ Sources/Imperial/Middleware/ImperialMiddleware.swift
@@ -1,8 +1,22 @@
 import Vapor
 
 public class ImperialMiddleware: Middleware {
+    let redirectPath: String?
+    
+    public init(redirect: String? = nil) {
+        self.redirectPath = redirect
+    }
+    
     public func respond(to request: Request, chainingTo next: Responder) throws -> Response {
-        _ = try request.getAccessToken()
-        return try next.respond(to: request)
+        do {
+            _ = try request.getAccessToken()
+            return try next.respond(to: request)
+        } catch let error {
+            if redirectPath != nil {
+                return Response(redirect: redirectPath!)
+            } else {
+                throw error
+            }
+        }
     }
 }
