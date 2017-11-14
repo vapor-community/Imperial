c361962f484519d4c86303f81d7c7d2d4a4c1fa9
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-14 09:44:15 -0600

    Created Google federated service class

---
+++ Sources/Imperial/Services/Google/Google.swift
@@ -0,0 +1,15 @@
+import HTTP
+
+public class Google: FederatedService {
+    public var auth: FederatedLoginService
+    public var router: FederatedServiceRouter
+    
+    @discardableResult
+    public required init(authenticate: String, callback: String, scope: [String: String] = [:], completion: @escaping (String) -> (ResponseRepresentable)) throws {
+        self.router = try GoogleRouter(callback: callback, completion: completion)
+        self.auth = self.router.service
+        
+        self.router.scope = scope
+        try self.router.configureRoutes(withAuthURL: authenticate)
+    }
+}
