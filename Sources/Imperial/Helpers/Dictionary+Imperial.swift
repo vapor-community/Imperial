c5e08713b2cb62bf58c2d8ae556de071d63137d9
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-14 09:37:49 -0600

    Created Dictionary.mergeValues extension method. Fixed .merge implementation

--- Sources/Imperial/Helpers/Dictionary+Imperial.swift
+++ Sources/Imperial/Helpers/Dictionary+Imperial.swift
@@ -1,5 +1,11 @@
 extension Dictionary where Key == String, Value == String {
     func merge(with separator: String) -> String {
-        return self.reduce(into: "") { $0 += [$1.key, separator, $1.value].joined(separator: " ") }
+        return self.reduce(into: [String]()) {
+                   $0.append([$1.key, separator, $1.value].joined())
+               }.joined(separator: " ")
+    }
+    
+    func mergeValues(with separator: String) -> String {
+        return self.reduce(into: [String]()) { $0.append($1.value) }.joined(separator: separator)
     }
 }
