d35b3bd44313eb5dc32a16b9a5b43eee65567174
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-28 09:28:08 -0600

    Removed FederatedAuthModel protocol

--- Sources/Imperial/Model/FederatedAuthModel.swift
+++ Sources/Imperial/Model/FederatedAuthModel.swift
@@ -1,13 +1,1 @@
 import FluentProvider
-
-public protocol FederatedAuthModel: Model {
-    var authTokenKey: String { get }
-    var encryptEnvKey: String { get }
-    var encryptKey: String { get }
-}
-
-extension FederatedAuthModel {
-    var encryptEnvKey: String {
-        return "imperial-access-encrypt"
-    }
-}
