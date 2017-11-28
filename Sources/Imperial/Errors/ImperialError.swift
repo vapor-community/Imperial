210e57065258ced66b8dd86ef8ec3add37d78877
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-28 10:09:15 -0600

    Created ImperialError.noServiceFound error case

--- Sources/Imperial/Errors/ImperialError.swift
+++ Sources/Imperial/Errors/ImperialError.swift
@@ -1,9 +1,11 @@
 public enum ImperialError: Error, CustomStringConvertible {
     case missingEnvVar(String)
+    case noServiceFound(String)
     
     public var description: String {
         switch self {
         case let .missingEnvVar(variable): return "Missing enviroment variable '\(variable)'"
+        case let .noServiceFound(name): return "No service was found with the name '\(name)'"
         }
     }
 }
