24f72797ba28c01b109c53fffaf16ffce8e5419b
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-28 10:34:41 -0600

    Made Dictionary extension methods public

--- Sources/Imperial/Helpers/Dictionary+Imperial.swift
+++ Sources/Imperial/Helpers/Dictionary+Imperial.swift
@@ -1,11 +1,11 @@
 extension Dictionary where Key == String, Value == String {
-    func merge(with separator: String) -> String {
+    public func merge(with separator: String) -> String {
         return self.reduce(into: [String]()) {
                    $0.append([$1.key, separator, $1.value].joined())
                }.joined(separator: " ")
     }
     
-    func mergeValues(with separator: String) -> String {
+    public func mergeValues(with separator: String) -> String {
         return self.reduce(into: [String]()) { $0.append($1.value) }.joined(separator: separator)
     }
 }
