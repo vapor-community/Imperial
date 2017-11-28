3ec84c30d12b91539120c803ec57d8aaea800c41
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-28 12:08:13 -0600

    Added dataUri property to FederatedInitializible protocol

--- Sources/Imperial/Model/FederatedInitializible.swift
+++ Sources/Imperial/Model/FederatedInitializible.swift
@@ -2,6 +2,7 @@ import JSON
 
 public protocol FederatedInitializible {
     var tokenPrefix: String { get }
+    var dataUri: String { get }
     
     static func create(with json: JSON) -> Self
 }
