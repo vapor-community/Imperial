05f042cf437fb6e3b0682e2fdc509584aefbf525
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-29 06:58:37 -0600

    Created ImperialError.missingJSONFromResponse case

--- Sources/Imperial/Errors/ImperialError.swift
+++ Sources/Imperial/Errors/ImperialError.swift
@@ -1,11 +1,13 @@
 public enum ImperialError: Error, CustomStringConvertible {
     case missingEnvVar(String)
     case noServiceFound(String)
+    case missingJSONFromResponse(String)
     
     public var description: String {
         switch self {
         case let .missingEnvVar(variable): return "Missing enviroment variable '\(variable)'"
         case let .noServiceFound(name): return "No service was found with the name '\(name)'"
+        case let .missingJSONFromResponse(uri): return "Reponse returned from '\(uri)' does not contain JSON"
         }
     }
 }
