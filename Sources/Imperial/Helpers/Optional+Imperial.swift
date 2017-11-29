4d55d1bd25e4a97b90621ff6731d59a0d30e8a28
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-29 12:19:01 -0600

    Documented Optional extension methods and operators

--- Sources/Imperial/Helpers/Optional+Imperial.swift
+++ Sources/Imperial/Helpers/Optional+Imperial.swift
@@ -1,4 +1,10 @@
 extension Optional {
+    
+    /// Gets the value contained in an optional.
+    ///
+    /// - Parameter error: The error to throw if the optional is `nil`.
+    /// - Returns: The value contained in the optional.
+    /// - Throws: The error passed in if the optional is `nil`.
     public func value(or error: Error)throws -> Wrapped {
         switch self {
         case let .some(value): return value
@@ -9,6 +15,13 @@ extension Optional {
 
 infix operator ??
 
+/// Unwrappes an optional and returns the value or throws an error if `nil`.
+///
+/// - Parameters:
+///   - lhs: The optional to unwrap.
+///   - rhs: The error to throw if the optional is `nil`.
+/// - Returns: The value that was contained in the optional.
+/// - Throws: The error passed in if the optional is `nil`.
 public func ??<T>(lhs: T?, rhs: Error)throws -> T {
     return try lhs.value(or: rhs)
 }
