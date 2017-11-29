a70b551baee9034871fccdfdf00889f2c250958f
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-29 12:42:10 -0600

    Documented methods for getting the access token from the request's session

--- Sources/Imperial/Helpers/Sessions+Imperial.swift
+++ Sources/Imperial/Helpers/Sessions+Imperial.swift
@@ -2,12 +2,24 @@ import Vapor
 import Sessions
 
 extension Request {
+    
+    /// Gets the access token from the current session.
+    ///
+    /// - Returns: The access token in the current session.
+    /// - Throws:
+    ///   - `Abort.unauthorized` if no access token exists.
+    ///   - `SessionsError.notConfigured` if session middlware is not configured yet.
     public func getAccessToken()throws -> String {
         return try self.assertSession().getAccessToken()
     }
 }
 
 extension Session {
+    
+    /// Gets the access token from the session.
+    ///
+    /// - Returns: The access token stored with the `access_token` key.
+    /// - Throws: `Abort.unauthorized` if no access token exists.m
     public func getAccessToken()throws -> String {
         guard let token = self.data["access_token"]?.string else {
             throw Abort(.unauthorized, reason: "User currently not authenticated")
