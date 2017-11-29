b3366d8c2c1522d1fd348c20f39bfe3bfb762579
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-29 06:40:53 -0600

    Uninstalled un-used package VaporOAuth

--- Package.swift
+++ Package.swift
@@ -8,11 +8,11 @@ let package = Package(
         .library(name: "Imperial", targets: ["Imperial"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/brokenhandsio/vapor-oauth.git", .exact("0.6.0")),
+        .package(url: "https://github.com/vapor/vapor.git", .exact("2.4.0")),
         .package(url: "https://github.com/vapor/fluent-provider.git", .exact("1.3.0"))
     ],
     targets: [
-        .target(name: "Imperial", dependencies: ["VaporOAuth", "FluentProvider"]),
+        .target(name: "Imperial", dependencies: ["Vapor", "FluentProvider"]),
         .testTarget(name: "ImperialTests", dependencies: ["Imperial"]),
     ]
 )
