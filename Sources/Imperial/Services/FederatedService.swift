6e6817373dc376cdc2f6de1fdf5efc6a8be17ed0
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-29 14:13:43 -0600

    Documented FederatedService protocol

--- Sources/Imperial/Services/FederatedService.swift
+++ Sources/Imperial/Services/FederatedService.swift
@@ -1,25 +1,45 @@
 import HTTP
 
-/*
- Usage:
+/**
+Represents a connection to an OAuth provider to get an access token for authenticating a user.
  
- class Service: FederatedService {
-     let auth: FederatedLoginService
-     let router: FederatedServiceRouter
- 
-     init(authenticate: String, callback: String, completion: @escaping (String) -> ())throws {
-         self.router = try Router(callback: callback)
-         self.auth = router.service
- 
-         drop.get(authenicate, router.authenticate)
-         drop.get(callback, router.callback)
-     }
- }
- */
+Usage:
 
+```swift
+import HTTP
+
+public class Service: FederatedService {
+    public var tokens: FederatedServiceTokens
+    public var router: FederatedServiceRouter
+
+    @discardableResult
+    public required init(authenticate: String, callback: String, scope: [String] = [], completion: @escaping (String) -> (ResponseRepresentable)) throws {
+        self.router = try ServiceRouter(callback: callback, completion: completion)
+        self.tokens = self.router.tokens
+
+        self.router.scope = scope
+        try self.router.configureRoutes(withAuthURL: authenticate)
+
+        Service.register(.service)
+    }
+}
+```
+ */
 public protocol FederatedService {
+    
+    /// The service's token model for getting the client ID and secret.
     var tokens: FederatedServiceTokens { get }
+    
+    /// The service's router for handling the request for the access token.
     var router: FederatedServiceRouter { get }
     
+    /// Creates a service for getting an access token from an OAuth provider.
+    ///
+    /// - Parameters:
+    ///   - authenticate: The path for the route that will redirect the user to the OAuth provider for authentication.
+    ///   - callback: The path (or URI) for the route that the provider will call when the user authenticates.
+    ///   - scope: The scopes to send to the provider to request access to.
+    ///   - completion: The completion handler that will fire at the end of the callback route. The access token is passed into the callback and the response that is returned will be returned from the callback route. This will usually be a redirect back to the app.
+    /// - Throws: Any errors that occur in the implementation.
     init(authenticate: String, callback: String, scope: [String], completion: @escaping (String) -> (ResponseRepresentable))throws
 }
