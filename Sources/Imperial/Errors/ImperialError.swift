5546169f1a3983bd7331079cb5ed96d40ae2efc1
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-05 06:24:21 -0600

    Created ImperialError.noExistingService error case

--- Sources/Imperial/Errors/ImperialError.swift
+++ Sources/Imperial/Errors/ImperialError.swift
@@ -8,6 +8,9 @@ public enum ImperialError: Error, CustomStringConvertible {
     /// Thrown when no service is registered with a given name.
     case noServiceFound(String)
     
+    /// Thrown when no `FederatedSewrvice` type is found whgen creating a `Service` from JSON.
+    case noExistingService(String)
+    
     /// Thrown when we attempt to create a `FederatedCreatable` model and there is
     /// no JSON in the response from the the request to `dataUri`.
     case missingJSONFromResponse(String)
@@ -23,6 +26,7 @@ public enum ImperialError: Error, CustomStringConvertible {
         switch self {
         case let .missingEnvVar(variable): return "Missing enviroment variable '\(variable)'"
         case let .noServiceFound(name): return "No service was found with the name '\(name)'"
+        case let .noExistingService(name): return "No service exists with the name '\(name)'"
         case let .missingJSONFromResponse(uri): return "Reponse returned from '\(uri)' does not contain JSON"
         case let .noServiceEndpoint(endpoint): return "Service does not have available endpoint for key '\(endpoint)'"
         case let .typeNotInitialized(type): return "No instence of type '\(type)' has been created"
