cb778ab29c177c0cac1631981c0267cb83aa89b3
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-06 13:53:59 -0600

    Documented Service+JSON file

--- Sources/Imperial/Services/Service/Service+JSON.swift
+++ Sources/Imperial/Services/Service/Service+JSON.swift
@@ -5,17 +5,27 @@ internal fileprivate(set) var federatedServices: [String: FederatedService.Type]
 ]
 
 extension FederatedService {
+    
+    /// Saves the `FederatedService` type
+    /// so it can be used as the `model` property in a service.
     public static func registerName() {
         let key = Self.typeName
         federatedServices[key] = self
     }
     
+    /// The description of the type as a `String`.
     public static var typeName: String {
         return String(describing: self)
     }
 }
 
+
 extension Service: JSONConvertible {
+    
+    /// Creates a JSON representation of the `Service`.
+    ///
+    /// - Returns: The `Service` in JSON format.
+    /// - Throws: Errors that occur when setting JSON keys.
     public func makeJSON() throws -> JSON {
         var json = JSON()
         try json.set("name", self.name)
@@ -25,6 +35,17 @@ extension Service: JSONConvertible {
         return json
     }
     
+    /// Creates a `Service` from a JSON object.
+    /// The JSON must contain the following keys:
+    /// - model: `String`
+    /// - name: `String`
+    /// - token_prefix: `String`
+    /// - endpoints: JSON object (`[String: String]`)
+    ///
+    /// - Parameter json: The JSON to create the `Service` with.
+    /// - Throws:
+    ///   - `ServiceError.noExistingService`: No service is registered with the key of the `model` parameter.
+    ///   - Errors from getting the values from JSON keys.
     public init(json: JSON) throws {
         let key: String = try json.get("model")
         
