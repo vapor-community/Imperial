224425f4c44a5c89a236d18107e8bcbae619ed38
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-01-18 17:12:07 -0600

    Replaced use of ResponseRepresentable with Future<ResponseEncodable> in Google and GitHub services

--- Sources/Imperial/Services/GitHub/GitHub.swift
+++ Sources/Imperial/Services/GitHub/GitHub.swift
@@ -1,11 +1,11 @@
-import HTTP
+import Vapor
 
 public class GitHub: FederatedService {
     public var tokens: FederatedServiceTokens
     public var router: FederatedServiceRouter
     
     @discardableResult
-    public required init(authenticate: String, callback: String, scope: [String] = [], completion: @escaping (String) -> (ResponseRepresentable)) throws {
+    public required init(authenticate: String, callback: String, scope: [String] = [], completion: @escaping (String) -> (Future<ResponseEncodable>)) throws {
         self.router = try GitHubRouter(callback: callback, completion: completion)
         self.tokens = self.router.tokens
         
--- Sources/Imperial/Services/Google/Google.swift
+++ Sources/Imperial/Services/Google/Google.swift
@@ -1,11 +1,11 @@
-import HTTP
+import Vapor
 
 public class Google: FederatedService {
     public var tokens: FederatedServiceTokens
     public var router: FederatedServiceRouter
     
     @discardableResult
-    public required init(authenticate: String, callback: String, scope: [String] = [], completion: @escaping (String) -> (ResponseRepresentable)) throws {
+    public required init(authenticate: String, callback: String, scope: [String] = [], completion: @escaping (String) -> (Future<ResponseEncodable>)) throws {
         self.router = try GoogleRouter(callback: callback, completion: completion)
         self.tokens = self.router.tokens
         
