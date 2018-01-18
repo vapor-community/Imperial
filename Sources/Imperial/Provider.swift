8930f95922ac81ef0fc5ad8f0f2fc54f2d08b4d9
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-01-18 17:23:51 -0600

    Updated provider for Vapor 3

--- Sources/Imperial/Provider.swift
+++ Sources/Imperial/Provider.swift
@@ -13,36 +13,9 @@ public class Provider: Vapor.Provider {
     
     /// Called after the container has initialized.
     public func boot(_ worker: Container) throws {
-       
+        Google.registerName()
+        GitHub.registerName()
     }
-//    public static var repositoryName: String = "Imperial"
-//
-//    public func boot(_ droplet: Droplet) throws {
-//        drop = droplet
-//        Google.registerName()
-//        GitHub.registerName()
-//    }
-//
-//    public func boot(_ config: Config) throws {
-//        guard let imperial = config["imperial"]?.object else {
-//            return
-//        }
-//
-//        if let ghID = imperial["github_client_id"]?.string,
-//           let ghSecret = imperial["github_client_secret"]?.string {
-//            ImperialConfig.gitHubID = ghID
-//            ImperialConfig.gitHubSecret = ghSecret
-//        }
-//
-//        if let googleID = imperial["google_client_id"]?.string,
-//           let googleSecret = imperial["google_client_secret"]?.string {
-//            ImperialConfig.googleID = googleID
-//            ImperialConfig.googleSecret = googleSecret
-//        }
-//    }
-//
-//    public func beforeRun(_ droplet: Droplet) throws {}
-//    public required init(config: Config) throws {}
 }
 
 internal struct ImperialConfig {
