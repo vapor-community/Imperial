408b5515678f029ceffe4de4630ae75c8dec28c4
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-01-20 12:21:54 -0600

    Removed use of ImperialConfig from GoogleAuth.init

--- Sources/Imperial/Services/Google/GoogleAuth.swift
+++ Sources/Imperial/Services/Google/GoogleAuth.swift
@@ -10,22 +10,7 @@ public class GoogleAuth: FederatedServiceTokens {
         let idError = ImperialError.missingEnvVar(idEnvKey)
         let secretError = ImperialError.missingEnvVar(secretEnvKey)
         
-        do {
-            guard let id = ImperialConfig.googleID else {
-                throw idError
-            }
-            self.clientID = id
-        } catch {
-            self.clientID = try Env.get(idEnvKey).value(or: idError)
-        }
-        
-        do {
-            guard let secret = ImperialConfig.googleSecret else {
-                throw secretError
-            }
-            self.clientSecret = secret
-        } catch {
-            self.clientSecret = try Env.get(secretEnvKey).value(or: secretError)
-        }
+        self.clientID = try Environment.get(idEnvKey) ?? idError
+        self.clientSecret = try Environment.get(secretEnvKey) ?? secretError
     }
 }
