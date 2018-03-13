1f322fa06c0edf04f1cba3f85e0ce4dcd74404d1
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-13 17:15:21 -0500

    Created Session getter and setter for coddle types

--- Sources/Imperial/Helpers/Sessions+Imperial.swift
+++ Sources/Imperial/Helpers/Sessions+Imperial.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Vapor
 
 extension Request {
@@ -24,5 +25,29 @@ extension Session {
             throw Abort(.unauthorized, reason: "User currently not authenticated")
         }
         return token
+    }
+    
+    /// Gets an object stored in a session with JSON as a given type.
+    ///
+    /// - Parameters:
+    ///   - key: The key for the object stored in the session, similar to a dictionary.
+    ///   - type: The type to convert the stored JSON to.
+    /// - Returns: The JSON from the session, decoded to the type passed in.
+    /// - Throws: Errors when no object is stored in the session with the given key, or decoding fails.
+    public func get<T>(_ key: String, as type: T.Type)throws -> T where T: Codable {
+        guard let stored = self[key] else {
+            throw Abort(.internalServerError, reason: "No element found in session with ket '\(key)'")
+        }
+        return try JSONDecoder().decode(T.self, from: Data(stored.utf8))
+    }
+    
+    /// Sets a key in the session to a codable object.
+    ///
+    /// - Parameters:
+    ///   - key: The key to store the object at, as you would in a dictionary.
+    ///   - data: The object to store.
+    /// - Throws: Errors that occur when encoding the object.
+    public func set<T>(_ key: String, to data: T)throws where T: Codable {
+        self[key] = try String(data: JSONEncoder().encode(data), encoding: .utf8)
     }
 }
