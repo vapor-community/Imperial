793b7fd21e3eae9aae227d927d4d81b2f09f5738
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-16 18:32:06 -0500

    Installed JWT package

--- Package.swift
+++ Package.swift
@@ -8,10 +8,11 @@ let package = Package(
         .library(name: "Imperial", targets: ["Imperial"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0")
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0-rc")
     ],
     targets: [
-        .target(name: "Imperial", dependencies: ["Vapor"]),
+        .target(name: "Imperial", dependencies: ["Vapor", "JWT"]),
         .testTarget(name: "ImperialTests", dependencies: ["Imperial"]),
     ]
 )
