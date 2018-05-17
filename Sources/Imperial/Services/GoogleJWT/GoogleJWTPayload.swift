d94b6c21a9d934252fcbceabacbf44aff32aaea1
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-16 19:29:42 -0500

    Created GoogleJWTPayload jwt payload struct

---
+++ Sources/Imperial/Services/GoogleJWT/GoogleJWTPayload.swift
@@ -0,0 +1,14 @@
+import Vapor
+import JWT
+
+public struct GoogleJWTPayload: JWTPayload {
+    public var iss: IssuerClaim
+    public var scope: String
+    public var aud: AudienceClaim
+    public var iat: IssuedAtClaim
+    public var exp: ExpirationClaim
+    
+    public func verify() throws {
+        try exp.verify()
+    }
+}
