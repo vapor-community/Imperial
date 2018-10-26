70ea5316eb79a9a9263797d0d4ed2408e2f59060
Tim <0xtimc@gmail.com>
2018-10-26 14:29:11 +0100

    Remove old code

--- Sources/Imperial/Services/Facebook/FacebookRouter.swift
+++ Sources/Imperial/Services/Facebook/FacebookRouter.swift
@@ -3,20 +3,14 @@ import Foundation
 
 public class FacebookRouter: FederatedServiceRouter {
     public let tokens: FederatedServiceTokens
-    public let callbackCompletion: (Request, String)throws -> (Future<ResponseEncodable>)
+    public let callbackCompletion: (Request, String) throws -> (Future<ResponseEncodable>)
     public var scope: [String] = []
     public let callbackURL: String
     public var accessTokenURL: String = "https://graph.facebook.com/v3.2/oauth/access_token"
     public var authURL: String {
-        //        return "https://accounts.google.com/o/oauth2/auth?" +
-        //            "client_id=\(self.tokens.clientID)&" +
-        //            "redirect_uri=\(self.callbackURL)&" +
-        //            "scope=\(scope.joined(separator: " "))&" +
-        //        "response_type=code"
         return "https://www.facebook.com/v3.2/dialog/oauth?" +
             "client_id=\(self.tokens.clientID)" +
-            "&redirect_uri=\(self.callbackURL)" /*+
-        "&state={state-param}"*/
+            "&redirect_uri=\(self.callbackURL)"
     }
 
     public required init(callback: String, completion: @escaping (Request, String)throws -> (Future<ResponseEncodable>)) throws {
