19c3748be487f5c2375977619a5e5136ca2a1650
David Muzi <david.muzi@shopify.com>
2019-01-19 16:14:23 -0500

    store nonce in session, make auth url public

--- Sources/Imperial/Services/Shopify/Session+Shopify.swift
+++ Sources/Imperial/Services/Shopify/Session+Shopify.swift
@@ -1,17 +1,26 @@
 import Vapor
 
 extension Session.Keys {
-	static let domain = "shop_domain"
+    static let domain = "shop_domain"
+    static let nonce = "nonce"
 }
 
 extension Session {
-	
-	public func shopDomain() throws -> String {
-		guard let domain = self[Keys.domain] else { throw Abort(.notFound) }
-		return domain
-	}
-	
-	func setShopDomain(_ domain: String) {
-		self[Keys.domain] = domain
-	}
+    
+    func shopDomain() throws -> String {
+        guard let domain = self[Keys.domain] else { throw Abort(.notFound) }
+        return domain
+    }
+    
+    func setShopDomain(_ domain: String) {
+        self[Keys.domain] = domain
+    }
+    
+    func setNonce(_ nonce: String?) {
+        self[Keys.nonce] = nonce
+    }
+    
+    func nonce() -> String? {
+        return self[Keys.nonce]
+    }
 }
--- Sources/Imperial/Services/Shopify/Shopify.swift
+++ Sources/Imperial/Services/Shopify/Shopify.swift
@@ -1,22 +1,26 @@
 import Vapor
 
 public class Shopify: FederatedService {
-	
-	public var tokens: FederatedServiceTokens {
-		return self.router.tokens
-	}
-	
-	public var router: FederatedServiceRouter
-	
-	public required init(router: Router,
-						 authenticate: String,
-						 callback: String,
-						 scope: [String],
-						 completion: @escaping (Request, String) throws -> (EventLoopFuture<ResponseEncodable>)) throws {
-		
-		self.router = try ShopifyRouter(callback: callback, completion: completion)
-		self.router.scope = scope
-		
-		try self.router.configureRoutes(withAuthURL: authenticate, on: router)
-	}
+    
+    public var tokens: FederatedServiceTokens {
+        return self.router.tokens
+    }
+    
+    public var router: FederatedServiceRouter {
+        return shopifyRouter
+    }
+    
+    public var shopifyRouter: ShopifyRouter
+    
+    public required init(router: Router,
+                         authenticate: String,
+                         callback: String,
+                         scope: [String],
+                         completion: @escaping (Request, String) throws -> (EventLoopFuture<ResponseEncodable>)) throws {
+        
+        self.shopifyRouter = try ShopifyRouter(callback: callback, completion: completion)
+        self.shopifyRouter.scope = scope
+        
+        try self.router.configureRoutes(withAuthURL: authenticate, on: router)
+    }
 }
