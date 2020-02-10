7220287f31c676db51f73f44ce2d8b89628eb9c8
Bastian Inuk Christensen <bastian@inuk.blog>
2020-02-10 19:15:51 +0100

    Updated Vapor, removed open crypto, it's part of Vapor

--- Package.swift
+++ Package.swift
@@ -1,24 +1,25 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
     name: "Imperial",
+    platforms: [
+       .macOS(.v10_14)
+    ],
     products: [
         .library(name: "Imperial", targets: ["Imperial"]),
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-alpha.3"),
-        .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0-alpha.1.1"),
-    .package(url: "https://github.com/vapor/open-crypto.git", from: "4.0.0-alpha.2")
+        .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0-alpha.1.1")
     ],
     targets: [
         .target(
             name: "Imperial",
             dependencies: [
                 "Vapor",
-                "JWTKit",
-                "OpenCrypto"
+                "JWTKit"
             ]
         ),
         .testTarget(name: "ImperialTests", dependencies: ["Imperial"]),
