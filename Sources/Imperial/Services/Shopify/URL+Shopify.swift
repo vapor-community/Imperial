2e4004cc6844341bd49a8a7c5a7003fe4359be09
Gwynne Raskind <gwynne@darkrainfall.org>
2020-03-13 15:43:15 -0500

    SwiftCrypto's version of MAC.description is not compatible with what OpenCrypto had. This is comprehensible; the type does _not_ claim a conformance to `LosslessStringConvertible`, so depending on its description to have any particular format is unsafe. Unfortunately, Vapor's implementation of the `hex` property for collections of UInt8 is a constrained extension on `Collection`, not `Sequence`, and `ContiguousBytes` is only the latter. Reimplemented it in much compressed form (also using a faster algorithm and the correct string encoding). Tests now pass.

--- Sources/Imperial/Services/Shopify/URL+Shopify.swift
+++ Sources/Imperial/Services/Shopify/URL+Shopify.swift
@@ -10,7 +10,7 @@ extension URL {
 		let queryString = queryItems.joined(separator: "&")
 		
         let hmac = HMAC<SHA256>.authenticationCode(for: queryString.bytes, using: .init(data: key.bytes))
-		return hmac.description
+		return hmac.hex
 	}
 
 	func isValidShopifyDomain() -> Bool {
@@ -20,4 +20,22 @@ extension URL {
 		
 		return absoluteString.range(of: "^[a-z0-9.-]+.myshopify.com$", options: .regularExpression) != nil
 	}
+}
+
+extension ContiguousBytes {
+    public var hex: String {
+        let table: [UInt8] = [
+            0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66
+        ]
+
+        return String(decoding: self.withUnsafeBytes { buffer in
+            Array<UInt8>.init(unsafeUninitializedCapacity: buffer.count * 2) { output, outCount in
+                outCount = buffer.reduce(into: 0) { count, byte in
+                    output[count + 0] = table[Int(byte / 16)]
+                    output[count + 1] = table[Int(byte % 16)]
+                    count += 2
+                }
+            }
+        }, as: Unicode.ASCII.self)
+    }
 }
