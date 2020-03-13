93fec51e7edce2ddbac6b2418ce929b66faf90dc
Gwynne Raskind <gwynne@darkrainfall.org>
2020-03-13 15:40:01 -0500

    Update manifest to 5.2 format. Require the RCs of Vapor and JWTKit as minimums, and get the new versions of everything.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version:5.1
-
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
@@ -11,15 +10,15 @@ let package = Package(
         .library(name: "Imperial", targets: ["Imperial"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-alpha.3"),
-        .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0-alpha.1.1")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc"),
+        .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0-rc")
     ],
     targets: [
         .target(
             name: "Imperial",
             dependencies: [
-                "Vapor",
-                "JWTKit"
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "JWTKit", package: "jwt-kit"),
             ]
         ),
         .testTarget(name: "ImperialTests", dependencies: ["Imperial"]),
