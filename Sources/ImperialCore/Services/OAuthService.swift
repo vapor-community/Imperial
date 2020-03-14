b55e443ccc7342a3f08c1f73ce49bfdb930c0332
Caleb Kleveter <caleb.kleveter@gmail.com>
2020-03-14 08:05:23 -0500

    Made registerde OAuthService instances thread safe

--- Sources/ImperialCore/Services/OAuthService.swift
+++ Sources/ImperialCore/Services/OAuthService.swift
@@ -1,23 +1,35 @@
+import class NIO.ThreadSpecificVariable
 import Vapor
 
-/// The services that are available for use in the application.
-/// Services are added and fetched with the `Service.register` and `.get` static methods.
-fileprivate var services: [String: OAuthService] = [:]
+fileprivate var services: ThreadSpecificVariable<OAuthServiceContainer> = .init(value: .init())
 
 /// Represents a service that interacts with an OAuth provider.
 public struct OAuthService: Codable, Content {
-    
+
+    /// The services that are available for use in the application.
+    /// Services are added and fetched with the `Service.register` and `.get` static methods.
+    private static var services: OAuthServiceContainer {
+        get { ImperialCore.services.currentValue! }
+        set { ImperialCore.services.currentValue  = newValue }
+    }
+
+
     /// The name of the service, i.e. "google", "github", etc.
     public let name: String
     
-    /// The prefix for the access token when it is used in a authorization header. Defaults to 'Bearer '.
+    /// The prefix for the access token when it is used in a authorization header. Defaults to `'Bearer '`.
     public let tokenPrefix: String
     
     /// The endpoints for the provider's API to use for initializing `FederatedCreatable` types
     public var endpoints: [String: String]
     
-    /// Creates an instance of a service.
-    /// This is is usually done by creating an extension and a static property.
+    /// Defines an OAuth provider that is supported by Imperial.
+    ///
+    /// Providers are usually defined in extensions as static properties.
+    ///
+    ///     extension OAuthService {
+    ///         static let google = OAuthService(name: "google", endpoints: [:])
+    ///     }
     ///
     /// - Parameters:
     ///   - name: The name of the service.
@@ -45,7 +57,7 @@ public struct OAuthService: Codable, Content {
     /// - Parameter service: The service to register.
     public static func register(_ service: OAuthService) {
         #warning("It would be nice if this method could be internal")
-        services[service.name] = service
+        self.services[service.name] = service
     }
     
     /// Gets a service if it is available for use.
@@ -54,6 +66,19 @@ public struct OAuthService: Codable, Content {
     /// - Returns: The service that matches the name passed in.
     /// - Throws: `ImperialError.noServiceFound` if no service is found with the name passed in.
     public static func get(service name: String) throws -> OAuthService {
-        return try services[name].value(or: ServiceError.noServiceFound(name))
+        return try self.services[name].value(or: ServiceError.noServiceFound(name))
+    }
+}
+
+private final class OAuthServiceContainer {
+    var services: [String: OAuthService]
+
+    init() {
+        self.services = [:]
+    }
+
+    subscript(service: String) -> OAuthService? {
+        get { self.services[service] }
+        set { self.services[service] = newValue }
     }
 }
