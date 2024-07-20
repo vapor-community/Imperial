0489c03d37e5d89e62edee57e992aed60d4af089
bgisme <55024704+bgisme@users.noreply.github.com>
2024-07-20 10:15:54 -0400

    Major updates. Extracted User.

--- Tests/ImperialTests/ImperialTests.swift
+++ Tests/ImperialTests/ImperialTests.swift
@@ -6,6 +6,8 @@ class ImperialTests: XCTestCase {
     
     func testPathComponents() {
         let callback = "http://localhost:8080/A/B/C"
+        XCTAssertEqual(callback.pathComponents(grouped: [":A"]).map{$0.description}.joined(), "ABC")
+        XCTAssertEqual(callback.pathComponents(grouped: ["*"]).map{$0.description}.joined(), "ABC")
         XCTAssertEqual(callback.pathComponents(grouped: ["A"]).map{$0.description}.joined(), "BC")
         XCTAssertEqual(callback.pathComponents(grouped: ["A", "B"]).map{$0.description}.joined(), "C")
         XCTAssertEqual(callback.pathComponents(grouped: ["A", "B", "C"]).map{$0.description}.joined(), "")
