7126c1fb640cbe6585158b32c1dc381372ee99c4
Francesco Paolo Severino <96546612+fpseverino@users.noreply.github.com>
2024-11-14 18:55:17 +0100

    Update Discord.swift
    Co-authored-by: Paul Toffoloni <69189821+ptoffy@users.noreply.github.com>

--- Sources/ImperialDiscord/Discord.swift
+++ Sources/ImperialDiscord/Discord.swift
@@ -7,7 +7,7 @@ public class Discord: FederatedService {
 
     @discardableResult
     public required init(
-        routes: any RoutesBuilder,
+        routes: some RoutesBuilder,
         authenticate: String,
         authenticateCallback: ((Request) async throws -> Void)?,
         callback: String,
