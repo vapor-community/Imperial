45634e6b70dfaccd46af3084514da89a9fc511b9
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2024-11-15 17:15:20 +0100

    Remove superfluous `String` extension

--- Sources/ImperialCore/Helpers/String+Tools.swift
+++ Sources/ImperialCore/Helpers/String+Tools.swift
@@ -1,18 +1,4 @@
 import Foundation
-import RoutingKit
-
-extension String {    
-	var pathComponents: [PathComponent] {
-		var pathComponentArray = [PathComponent]()
-		if let components = URL(string: self)?.pathComponents {
-			for item in components where item != "/" {
-				pathComponentArray.append(PathComponent(stringLiteral: item))
-			}
-		}
-		return pathComponentArray
-	}
-
-}
 
 extension String.UTF8View: DataProtocol {
     public var regions: CollectionOfOne<Array<UInt8>> { Array(self).regions }
