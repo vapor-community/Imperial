c9199fc56f03879f68ab9819bfdeff239871c3a3
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2024-11-16 13:30:01 +0100

    Add more basic tests

--- Sources/ImperialCore/Errors/ServiceError.swift
+++ Sources/ImperialCore/Errors/ServiceError.swift
@@ -2,7 +2,6 @@
 public struct ServiceError: Error, Sendable, Equatable {
     public struct ErrorType: Sendable, Hashable, CustomStringConvertible, Equatable {
         enum Base: String, Sendable, Equatable {
-            case noServiceFound
             case noServiceEndpoint
         }
 
@@ -12,7 +11,6 @@ public struct ServiceError: Error, Sendable, Equatable {
             self.base = base
         }
 
-        public static let noServiceFound = Self(.noServiceFound)
         public static let noServiceEndpoint = Self(.noServiceEndpoint)
 
         public var description: String {
@@ -22,16 +20,13 @@ public struct ServiceError: Error, Sendable, Equatable {
 
     private struct Backing: Sendable, Equatable {
         fileprivate let errorType: ErrorType
-        fileprivate let name: String?
         fileprivate let endpoint: String?
 
         init(
             errorType: ErrorType,
-            name: String? = nil,
             endpoint: String? = nil
         ) {
             self.errorType = errorType
-            self.name = name
             self.endpoint = endpoint
         }
 
@@ -43,16 +38,10 @@ public struct ServiceError: Error, Sendable, Equatable {
     private var backing: Backing
 
     public var errorType: ErrorType { backing.errorType }
-    public var name: String? { backing.name }
     public var endpoint: String? { backing.endpoint }
 
     private init(backing: Backing) {
         self.backing = backing
-    }
-
-    /// Thrown when no service is registered with a given name.
-    public static func noServiceFound(_ name: String) -> Self {
-        .init(backing: .init(errorType: .noServiceFound, name: name))
     }
 
     /// Thrown when a `FederatedCreatable` type has a `serviceKey` that does not match any available endpoints in the service.
@@ -68,10 +57,6 @@ public struct ServiceError: Error, Sendable, Equatable {
 extension ServiceError: CustomStringConvertible {
     public var description: String {
         var result = #"ServiceError(errorType: \#(self.errorType)"#
-
-        if let name {
-            result.append(", no service was found with the name: \(name)")
-        }
 
         if let endpoint {
             result.append(", service does not have available endpoint for key: \(endpoint)")
--- Tests/ImperialTests/ImperialTests.swift
+++ Tests/ImperialTests/ImperialTests.swift
@@ -1,81 +1,29 @@
-import ImperialGitHub
-import ImperialGoogle
-import ImperialFacebook
-import ImperialKeycloak
-import ImperialDiscord
 import ImperialAuth0
+import ImperialDiscord
+import ImperialDropbox
+import ImperialFacebook
+import ImperialGitHub
+import ImperialGitlab
+import ImperialGoogle
+import ImperialKeycloak
+import ImperialMicrosoft
 import Testing
+import XCTVapor
 
 @testable import ImperialCore
 
 @Suite("ImperialCore Tests")
 struct ImperialTests {
-    @Test("ImperialError & ServiceError")
-    func errors() {
-        #expect(ImperialError.missingEnvVar("test").description == "ImperialError(errorType: missingEnvVar, missing enviroment variable: test)")
-        #expect(ImperialError.missingEnvVar("foo") == ImperialError.missingEnvVar("bar"))
-
-        #expect(ServiceError.noServiceFound("test").description == "ServiceError(errorType: noServiceFound, no service was found with the name: test)")
-        #expect(ServiceError.noServiceEndpoint("test").description == "ServiceError(errorType: noServiceEndpoint, service does not have available endpoint for key: test)")
-        #expect(ServiceError.noServiceFound("foo") == ServiceError.noServiceFound("bar"))
-        #expect(ServiceError.noServiceEndpoint("foo") == ServiceError.noServiceEndpoint("bar"))
-    }
-
-    @Test("GitHub Route")
-    func githubRoute() async throws {
+    @Test("Auth0 Route")
+    func auth0Route() async throws {
         try await withApp { app in
-            try await app.test(.GET, "/github", afterResponse: { res async throws in
-				#expect(res.status == .notFound)
-			})
-
-            try app.oAuth(from: GitHub.self, authenticate: "github", callback: "gh-auth-complete", redirect: "/")
-
-            try await app.test(.GET, "/github", afterResponse: { res async throws in
-				#expect(res.status != .notFound)
-			})
-        }
-    }
-
-    @Test("Google Route")
-    func googleRoute() async throws {
-        try await withApp { app in
-            try await app.test(.GET, "/google", afterResponse: { res async throws in
-				#expect(res.status == .notFound)
-			})
-
-            try app.oAuth(from: Google.self, authenticate: "google", callback: "google-auth-complete", redirect: "/")
-
-            try await app.test(.GET, "/google", afterResponse: { res async throws in
-				#expect(res.status != .notFound)
-			})
-        }
-    }
-
-    @Test("Facebook Route")
-    func facebookRoute() async throws {
-        try await withApp { app in
-            try await app.test(.GET, "/facebook", afterResponse: { res async throws in
+            try await app.test(.GET, "/auth0", afterResponse: { res async throws in
                 #expect(res.status == .notFound)
             })
 
-            try app.oAuth(from: Facebook.self, authenticate: "facebook", callback: "facebook-auth-complete", redirect: "/")
+            try app.oAuth(from: Auth0.self, authenticate: "auth0", callback: "auth0-auth-complete", redirect: "/")
 
-            try await app.test(.GET, "/facebook", afterResponse: { res async throws in
-                #expect(res.status != .notFound)
-            })
-        }
-    }
-
-    @Test("Keycloak Route")
-    func keycloakRoute() async throws {
-        try await withApp { app in
-            try await app.test(.GET, "/keycloak", afterResponse: { res async throws in
-                #expect(res.status == .notFound)
-            })
-
-            try app.oAuth(from: Keycloak.self, authenticate: "keycloak", callback: "keycloak-auth-complete", redirect: "/")
-
-            try await app.test(.GET, "/keycloak", afterResponse: { res async throws in
+            try await app.test(.GET, "/auth0", afterResponse: { res async throws in
                 #expect(res.status != .notFound)
             })
         }
@@ -96,18 +44,132 @@ struct ImperialTests {
         }
     }
 
-    @Test("Auth0 Route")
-    func auth0Route() async throws {
+    @Test("Dropbox Route")
+    func dropboxRoute() async throws {
         try await withApp { app in
-            try await app.test(.GET, "/auth0", afterResponse: { res async throws in
+            try await app.test(.GET, "/dropbox", afterResponse: { res async throws in
                 #expect(res.status == .notFound)
             })
 
-            try app.oAuth(from: Auth0.self, authenticate: "auth0", callback: "auth0-auth-complete", redirect: "/")
+            try app.oAuth(from: Dropbox.self, authenticate: "dropbox", callback: "dropbox-auth-complete", redirect: "/")
 
-            try await app.test(.GET, "/auth0", afterResponse: { res async throws in
+            try await app.test(.GET, "/dropbox", afterResponse: { res async throws in
                 #expect(res.status != .notFound)
             })
         }
+    }
+
+    @Test("Facebook Route")
+    func facebookRoute() async throws {
+        try await withApp { app in
+            try await app.test(.GET, "/facebook", afterResponse: { res async throws in
+                #expect(res.status == .notFound)
+            })
+
+            try app.oAuth(from: Facebook.self, authenticate: "facebook", callback: "facebook-auth-complete", redirect: "/")
+
+            try await app.test(.GET, "/facebook", afterResponse: { res async throws in
+                #expect(res.status != .notFound)
+            })
+        }
+    }
+
+    @Test("GitHub Route")
+    func githubRoute() async throws {
+        try await withApp { app in
+            try await app.test(.GET, "/github", afterResponse: { res async throws in
+				#expect(res.status == .notFound)
+			})
+
+            try app.oAuth(from: GitHub.self, authenticate: "github", callback: "gh-auth-complete", redirect: "/")
+
+            try await app.test(.GET, "/github", afterResponse: { res async throws in
+				#expect(res.status != .notFound)
+			})
+        }
+    }
+
+    @Test("Gitlab Route")
+    func gitlabRoute() async throws {
+        try await withApp { app in
+            try await app.test(.GET, "/gitlab", afterResponse: { res async throws in
+                #expect(res.status == .notFound)
+            })
+
+            try app.oAuth(from: Gitlab.self, authenticate: "gitlab", callback: "gitlab-auth-complete", redirect: "/")
+
+            try await app.test(.GET, "/gitlab", afterResponse: { res async throws in
+                #expect(res.status != .notFound)
+            })
+        }
+    }
+
+    @Test("Google Route")
+    func googleRoute() async throws {
+        try await withApp { app in
+            try await app.test(.GET, "/google", afterResponse: { res async throws in
+				#expect(res.status == .notFound)
+			})
+
+            try app.oAuth(from: Google.self, authenticate: "google", callback: "google-auth-complete", redirect: "/")
+
+            try await app.test(.GET, "/google", afterResponse: { res async throws in
+				#expect(res.status != .notFound)
+			})
+        }
+    }
+
+    @Test("Google JWT Route")
+    func googleJWTRoute() async throws {
+        try await withApp { app in
+            try await app.test(.GET, "/googleJWT", afterResponse: { res async throws in
+                #expect(res.status == .notFound)
+            })
+
+            try app.oAuth(from: GoogleJWT.self, authenticate: "googleJWT", callback: "googleJWT-auth-complete", redirect: "/")
+
+            try await app.test(.GET, "/googleJWT", afterResponse: { res async throws in
+                #expect(res.status != .notFound)
+            })
+        }
+    }
+
+    @Test("Keycloak Route")
+    func keycloakRoute() async throws {
+        try await withApp { app in
+            try await app.test(.GET, "/keycloak", afterResponse: { res async throws in
+                #expect(res.status == .notFound)
+            })
+
+            try app.oAuth(from: Keycloak.self, authenticate: "keycloak", callback: "keycloak-auth-complete", redirect: "/")
+
+            try await app.test(.GET, "/keycloak", afterResponse: { res async throws in
+                #expect(res.status != .notFound)
+            })
+        }
+    }
+
+    @Test("Microsoft Route")
+    func microsoftRoute() async throws {
+        try await withApp { app in
+            try await app.test(.GET, "/microsoft", afterResponse: { res async throws in
+                #expect(res.status == .notFound)
+            })
+
+            try app.oAuth(from: Microsoft.self, authenticate: "microsoft", callback: "microsoft-auth-complete", redirect: "/")
+
+            try await app.test(.GET, "/microsoft", afterResponse: { res async throws in
+                #expect(res.status != .notFound)
+            })
+        }
+    }
+
+    @Test("ImperialError & ServiceError")
+    func errors() {
+        #expect(ImperialError.missingEnvVar("test").description == "ImperialError(errorType: missingEnvVar, missing enviroment variable: test)")
+        #expect(ImperialError.missingEnvVar("foo") == ImperialError.missingEnvVar("bar"))
+
+        #expect(ServiceError.noServiceEndpoint("test").description == "ServiceError(errorType: noServiceEndpoint, service does not have available endpoint for key: test)")
+        #expect(ServiceError.noServiceEndpoint("foo") == ServiceError.noServiceEndpoint("bar"))
     }
 }
