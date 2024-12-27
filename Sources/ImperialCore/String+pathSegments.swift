5c85e7d34a6ec7903b55f1672025e5fe32d6575b
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2024-12-27 17:53:11 +0100

    Make the linter happy

--- Sources/ImperialCore/String+pathSegments.swift
+++ Sources/ImperialCore/String+pathSegments.swift
@@ -3,16 +3,16 @@ import RoutingKit
 
 extension String {
     var pathSegments: [PathComponent] {
-		if let components = URL(string: self)?.pathComponents {
-			var pathSegments: [PathComponent] = []
+        if let components = URL(string: self)?.pathComponents {
+            var pathSegments: [PathComponent] = []
 
-			for component in components where component != "/" {
-				pathSegments.append(PathComponent(stringLiteral: component))
-			}
+            for component in components where component != "/" {
+                pathSegments.append(PathComponent(stringLiteral: component))
+            }
 
-			return pathSegments
-		} else {
-			return self.pathComponents
-		}
+            return pathSegments
+        } else {
+            return self.pathComponents
+        }
     }
 }
