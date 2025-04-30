3bba7447aafb9ae72b6c8ab235292821714d2291
bgisme <55024704+bgisme@users.noreply.github.com>
2025-04-30 16:31:13 -0400

    Updated Google + GoogleRouter • removed unnecessary properties • updated initializer to match protocol • create url query items in initializer • replace `authURL()` with `authURLComponents()`

--- Sources/ImperialGoogle/Standard/Google.swift
+++ Sources/ImperialGoogle/Standard/Google.swift
@@ -8,10 +8,10 @@ public struct Google: FederatedService {
         authenticate: String,
         authenticateCallback: (@Sendable (Request) async throws -> Void)?,
         callback: String,
-        scope: [String] = [],
-        completion: @escaping @Sendable (Request, String) async throws -> some AsyncResponseEncodable
+        queryItems: [URLQueryItem] = [],
+        completion: @escaping @Sendable (Request, String, ByteBuffer?) async throws -> some AsyncResponseEncodable
     ) throws {
-        try GoogleRouter(callback: callback, scope: scope, completion: completion)
+        try GoogleRouter(callback: callback, queryItems: queryItems, completion: completion)
             .configureRoutes(withAuthURL: authenticate, authenticateCallback: authenticateCallback, on: routes)
     }
 }
