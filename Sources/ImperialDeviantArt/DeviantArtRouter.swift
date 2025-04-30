a1177a3aacf6e34731542842d3e29012c6771f02
bgisme <55024704+bgisme@users.noreply.github.com>
2025-04-30 16:25:58 -0400

    Updated DeviantArt • removed unnecessary properties • updated initializer to match protocol • create url query items in initializer • replace `authURL()` with `authURLComponents()` • change `fetchToken()` to `fetchTokenAndResponse()`

--- Sources/ImperialDeviantArt/DeviantArtRouter.swift
+++ Sources/ImperialDeviantArt/DeviantArtRouter.swift
@@ -2,35 +2,38 @@ import Foundation
 import Vapor
 
 struct DeviantArtRouter: FederatedServiceRouter {
+    /// FederatedServiceRouter properties
     let tokens: any FederatedServiceTokens
-    let callbackCompletion: @Sendable (Request, String) async throws -> any AsyncResponseEncodable
-    let scope: [String]
+    let callbackCompletion: @Sendable (Request, String, ByteBuffer?) async throws -> any AsyncResponseEncodable
     let callbackURL: String
     let accessTokenURL: String = "https://www.deviantart.com/oauth2/token"
+    /// Local properties
+    let queryItems: [URLQueryItem]
 
     init(
-        callback: String, scope: [String], completion: @escaping @Sendable (Request, String) async throws -> some AsyncResponseEncodable
+        callback: String, queryItems: [URLQueryItem], completion: @escaping @Sendable (Request, String, ByteBuffer?) async throws -> some AsyncResponseEncodable
     ) throws {
         self.tokens = try DeviantArtAuth()
         self.callbackURL = callback
         self.callbackCompletion = completion
-        self.scope = scope
+        self.queryItems = queryItems + [
+            .codeResponseTypeItem,
+            .init(clientID: tokens.clientID),
+            .init(redirectURIItem: callback),
+        ]
     }
 
-    func authURL(_ request: Request) throws -> String {
-        let scope: String
-        if self.scope.count > 0 {
-            scope = "scope=" + self.scope.joined(separator: " ") + "&"
-        } else {
-            scope = ""
-        }
-        return "https://www.deviantart.com/oauth2/authorize?"
-            + "client_id=\(self.tokens.clientID)&"
-            + "redirect_uri=\(self.callbackURL)&\(scope)"
-            + "response_type=code"
+    func authURLComponents(_ request: Request) throws -> URLComponents {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "www.deviantart.com"
+        components.path = "/oauth2/authorize"
+        components.queryItems = self.queryItems
+        return components
     }
 
-    func fetchToken(from request: Request) async throws -> String {
+    func fetchTokenAndResponseBody(from request: Request) async throws -> (String, ByteBuffer?) {
+        try verifyState(request)
         let code: String
         if let queryCode: String = try request.query.get(at: codeKey) {
             code = queryCode
@@ -49,7 +52,8 @@ struct DeviantArtRouter: FederatedServiceRouter {
         let refresh = try response.content.get(String.self, at: ["refresh_token"])
         request.session.setRefreshToken(refresh)
 
-        return try response.content.get(String.self, at: ["access_token"])
+        let token = try response.content.get(String.self, at: ["access_token"])
+        return (token, response.body)
     }
 
     func callbackBody(with code: String) -> any AsyncResponseEncodable {
